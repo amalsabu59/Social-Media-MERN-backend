d6acca59e206d8b7629fd83fe4e92f0813f7a26e
amalsabu59 <amalsabu59@gmail.com>
2022-02-19 21:33:29 +0530

    CRUD delete user added

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,7 @@ router.get("/",(req,res)=>{
 
 //update user
 router.put("/:id", async (req,res) => {
-    if(req.body.userId === req.params.id || req.user.isAdmin){
+    if(req.body.userId === req.params.id || req.body.isAdmin){
         if(req.body.password){
             try{
                 const salt = await bcrypt.genSalt(10)
@@ -35,6 +35,22 @@ router.put("/:id", async (req,res) => {
 
 
 //delete user
+
+router.delete("/:id", async (req,res) => {
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+            try {
+                await User.findByIdAndDelete(req.params.id)
+                res.status(200).json("Account has been deleted")
+            } catch(err){
+                return res.status(500).json(err);
+            }
+        }else {
+        return res.status(403).json("you can only delete account")
+    }
+})
+
+
+
 //get a user
 //follow a user
 //unfollow a user
