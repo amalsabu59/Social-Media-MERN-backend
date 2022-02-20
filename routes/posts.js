f83846d535d388e6601a1ac3e62755900eee0c8a
amalsabu59 <amalsabu59@gmail.com>
2022-02-20 16:53:27 +0530

    posts CRED operations added

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,78 @@
 const router = require("express").Router();
+const Post = require("../models/posts")
 
-router.get("/",(req,res)=>{
-    console.log("posts");
+//create a post
+router.post("/",async(req,res)=>{
+    const newPost = new Post(req.body);
+    try{
+        const savePost = await newPost.save();
+        res.status(200).json(savePost)
+
+    }catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+
+//update a post 
+
+router.put("/:id",async(req,res)=>{
+    try{
+        const post = await Post.findById(req.params.id);
+        if (post.userId === req.body.userId){
+            await post.updateOne({$set:req.body});
+            res.status(200).json("the post has been updated")
+        }req.status(403).json("you can update only your post")
+
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+//deleate a post
+
+router.delete("/:id",async(req,res)=>{
+    try{
+        const post = await Post.findById(req.params.id);
+        if (post.userId === req.body.userId){
+            await post.deleteOne();
+            res.status(200).json("the post has been deleted")
+        }req.status(403).json("you can delete only your post")
+
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+
+//like post
+
+router.put("/:id/like", async (req,res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+        if (!post.likes.includes(req.body.userId)){
+            await post.updateOne({ $push: { likes:req.body.userId}})
+            res.status(200).json("the posthas been likes")
+        }else {
+            await post.updateOne({$pull:{likes:req.body.userId}})
+            res.status(200).json("The post has been disliked")
+        }
+    }catch(err){
+        res.status(500).json(err);
+    }
 })
+
+//get a post
+
+router.get("/:id",async (req,res)=>{
+    try{
+        const post = await Post.findById(req.params.id)
+        res.status(200).json(post)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//get timeline posts
 
 module.exports = router
