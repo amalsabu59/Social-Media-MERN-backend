7735e3d08000cfde634e46d77658d76471f112ec
amalsabu59 <amalsabu59@gmail.com>
2022-04-17 23:13:20 +0530

    login issue fixed

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/user")
 const bcrypt = require("bcrypt");
-const { response } = require("express");
+
 
 //REGISTER
 
@@ -34,21 +34,24 @@ router.post("/register",async(req,res) => {
 });
 
 //LOGIN
-router.post("/login",async(req,res)=>{
-try {
-    const user = await User.findOne({ email:req.body.email })
-    !user && res.status(404).json("user not found")
-
-    const validPassword = await bcrypt.compare(req.body.password,user.password)
-    !validPassword && res.status(400).json("wrong password")
-    
-    res.status(200).json(user)
-} catch (err) {
-    response.status(500).json(err)
-}
-  
-
-})
+router.post("/login", async (req, res) => {
+    try {
+      const user = await User.findOne({ email: req.body.email });
+      !user && res.status(404).json("user not found");
+      if(user)
+      {
+          const validPassword = await bcrypt.compare(req.body.password, user.password)
+          !validPassword && res.status(400).json("wrong password")
+          if(validPassword){
+            const { password, ...others } = user._doc;
+              res.status(200).json(others)
+          }
+      }  
+         
+    } catch (err) {
+      res.status(500).send({message:err})
+    }
+  });
 
 
 module.exports = router
