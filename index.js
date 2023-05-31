6b4911efd989c80b6fb2333634612473cac0ecbb
amalsabu59 <amalsabu59@gmail.com>
2023-05-31 21:02:59 +0530

    mongo err fixed

--- index.js
+++ index.js
@@ -4,39 +4,37 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const userRoutes = require("./routes/users")
-const authRoutes = require("./routes/auth")
-const postRoutes = require("./routes/posts")
-const conversationRoutes = require("./routes/conversations")
-const messageRoutes = require("./routes/messages")
-const multer = require("multer")
-const path = require("path")
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
+const postRoutes = require("./routes/posts");
+const conversationRoutes = require("./routes/conversations");
+const messageRoutes = require("./routes/messages");
+const multer = require("multer");
+const path = require("path");
 
-dotenv.config()
+dotenv.config();
 
+mongoose
+  .connect(
+    "mongodb+srv://admin_user:UHXQTa7gLkvKVedf@cluster0.tpfuc.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("connected  to mongodb !!"))
 
-mongoose.connect(process.env.connection_url, {
-   
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    
+  .catch((err) => console.log(err.message));
 
-    
-  }).then(()=> console.log("connected  to mongodb !!"))
-
-    .catch(err => console.log(err.message));
-
-
-
-app.use("/images/person",express.static(path.join(__dirname,"public/images/person")))
+app.use(
+  "/images/person",
+  express.static(path.join(__dirname, "public/images/person"))
+);
 //Middleware
 
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
-
-
-
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -56,13 +54,13 @@ app.post("/api/uploaddp", upload.single("file"), (req, res) => {
   }
 });
 
-app.use("/api/users",userRoutes);
-app.use("/api/auth",authRoutes);
-app.use("/api/posts",postRoutes);
-app.use("/api/posts",postRoutes);
-app.use("/api/conversations",conversationRoutes);
-app.use("/api/messages",messageRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/conversations", conversationRoutes);
+app.use("/api/messages", messageRoutes);
 
-app.listen(8800,()=>{
-    console.log("backend server is running !!");
-})
+app.listen(8800, () => {
+  console.log("backend server is running !!");
+});
